de22240c82652f4a12b4aa4aede58dfba8a985d6
Ellie Buehler <ellieabuehler@gmail.com>
2022-01-03 11:59:53 -0800

    got images loading

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@ module.exports = {
   images: {
     domains: ['static.gotsby.org', 'static.ghost.org','appliedwebdesigns.com'],
     loader: "imgix",
-    path: "https://noop/",
+    path: '',
   },
 }
